7e94815d6c48141b8ac241da269a6d32389a9969
blu <encode.dk>
2013-11-21 18:37:28 +0200

    Preview generation service: logging

--- jphoto/src/core/services/conversion/FileConversionServiceImpl.java
+++ jphoto/src/core/services/conversion/FileConversionServiceImpl.java
@@ -1,6 +1,7 @@
 package core.services.conversion;
 
 import core.general.conversion.ConversionOptions;
+import core.log.LogHelper;
 import utils.ShellUtils;
 
 import java.awt.*;
@@ -9,14 +10,18 @@ import java.io.IOException;
 
 public class FileConversionServiceImpl implements FileConversionService {
 
+	private final LogHelper log  = new LogHelper( FileConversionServiceImpl.class );
+
 	@Override
 	public void convert( final File sourceFile, final File destinationFile, final ConversionOptions conversionOptions ) throws IOException, InterruptedException {
 
-		String resize = dimensionToCovertString( conversionOptions.getDimension() );
+		final String resize = dimensionToCovertString( conversionOptions.getDimension() );
 		final String cmd = String.format( "convert -alpha off -strip +profile iptc -density %s -units PixelsPerInch -resize %s %s %s"
 				, conversionOptions.getDensity(), resize, sourceFile.getPath(), destinationFile.getPath() );
 
-		ShellUtils.executeCommand( cmd );
+		final String result = ShellUtils.executeCommand( cmd );
+
+		log.debug( result );
 	}
 
 	private static String dimensionToCovertString( Dimension dimension ) {
