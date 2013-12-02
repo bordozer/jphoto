06f6bedc422a2878701a24c250bc377245532415
blu <encode.dk>
2013-12-02 17:53:57 +0200

    NotificationServiceImpl - 6

--- jphoto/src/core/services/notification/text/message/NewPhotoOfSignedAuthorPrivateMessageTextStrategy.java
+++ jphoto/src/core/services/notification/text/message/NewPhotoOfSignedAuthorPrivateMessageTextStrategy.java
@@ -16,12 +16,12 @@ public class NewPhotoOfSignedAuthorPrivateMessageTextStrategy extends AbstractNo
 
 	@Override
 	public String getNotificationSubject() {
-		return String.format( "New Photo Of Signed Author Private Message - Subject: %s has uploaded new photo '%s'", getPhotoAuthor(), services.getEntityLinkUtilsService().getPhotoCardLink( photo ) );
+		return String.format( "New Photo Of Signed Author - Private Message Subject: %s has uploaded new photo '%s'", services.getEntityLinkUtilsService().getUserCardLink( getPhotoAuthor() ), services.getEntityLinkUtilsService().getPhotoCardLink( photo ) );
 	}
 
 	@Override
 	public String getNotificationText() {
-		return String.format( "New Photo Of Signed Author Private Message - Body: %s has uploaded new photo '%s'", getPhotoAuthor(), services.getEntityLinkUtilsService().getPhotoCardLink( photo ) );
+		return String.format( "New Photo Of Signed Author - Private Message Body: %s has uploaded new photo '%s'", services.getEntityLinkUtilsService().getUserCardLink( getPhotoAuthor() ), services.getEntityLinkUtilsService().getPhotoCardLink( photo ) );
 	}
 
 	private User getPhotoAuthor() {
