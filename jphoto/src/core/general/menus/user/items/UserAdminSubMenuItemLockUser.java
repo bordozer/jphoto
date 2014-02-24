dffd68b213ab9f9aa7a7b9719dfe3366ba90629a
blu <hhh@djfjf.ru>
2014-02-24 21:16:35 +0200

    ...

--- jphoto/src/core/general/menus/user/items/UserAdminSubMenuItemLockUser.java
+++ jphoto/src/core/general/menus/user/items/UserAdminSubMenuItemLockUser.java
@@ -4,7 +4,6 @@ import core.general.menus.AbstractEntryMenuItemCommand;
 import core.general.menus.EntryMenuOperationType;
 import core.general.menus.user.AbstractUserMenuItem;
 import core.general.user.User;
-import core.services.security.SecurityService;
 import core.services.security.Services;
 import utils.TranslatorUtils;
 
