beace44150b4d83f6bb30a357e8acd8e58810cb0
blu <encode.dk>
2014-03-27 14:02:00 +0200

    Logged user: change UI language

--- jphoto/src/controllers/language/ChangeUiLanguageController.java
+++ jphoto/src/controllers/language/ChangeUiLanguageController.java
@@ -2,6 +2,8 @@ package controllers.language;
 
 import core.context.EnvironmentContext;
 import core.services.translator.Language;
+import core.services.translator.TranslatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +15,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping( value = "language" )
 public class ChangeUiLanguageController {
 
+	@Autowired
+	private TranslatorService translatorService;
+
 	@RequestMapping( method = RequestMethod.GET, value = "{languageCode}/" )
 	@ResponseStatus( value = HttpStatus.OK )
 	public void commentsToUser( final @PathVariable( "languageCode" ) String languageCode ) {
 		final Language language = Language.getByCode( languageCode );
 
 		EnvironmentContext.getCurrentUser().setLanguage( language );
+
+		EnvironmentContext.getEnvironment().setHiMessage( translatorService.translate( "The UI language has been changed for this session only. "
+					+ "Set preferred a language in your profile settings for permanent basement", language ) );
 	}
 }
