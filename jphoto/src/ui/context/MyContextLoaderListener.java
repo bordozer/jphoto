20636ae562e2836598d5416d7869f84b8342542b
blu <encode.dk>
2014-04-14 10:40:48 +0300

    context start loggint

---
+++ jphoto/src/ui/context/MyContextLoaderListener.java
@@ -0,0 +1,13 @@
+package ui.context;
+
+import org.springframework.web.context.ContextLoaderListener;
+
+public class MyContextLoaderListener extends ContextLoaderListener {
+
+	public MyContextLoaderListener() {
+
+		System.out.println( "==================================================================" );
+		System.out.println( "                     SPRING CONTEXT START                         " );
+		System.out.println( "==================================================================" );
+	}
+}
