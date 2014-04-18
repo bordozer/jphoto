1982bb3466aa64269ecd4ad756c8dc0eb3238f36
blu <encode.dk>
2014-04-18 08:59:28 +0300

    clean up

--- jphoto/src/json/admin/user/lock/UserLockHistoryController.java
+++ jphoto/src/json/admin/user/lock/UserLockHistoryController.java
@@ -1,8 +1,5 @@
 package json.admin.user.lock;
 
-import core.services.user.UserService;
-import core.services.utils.EntityLinkUtilsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +14,9 @@ import static com.google.common.collect.Lists.newArrayList;
 @Controller
 public class UserLockHistoryController {
 
-//	@Autowired
-//	private UserService userService;
-
-//	@Autowired
-//	private EntityLinkUtilsService entityLinkUtilsService;
-
 	@RequestMapping( method = RequestMethod.GET, value = "/", produces = "application/json" )
 	@ResponseBody
 	public List<UserLockHistoryDTO> userCardVotingAreas( final @PathVariable( "userId" ) int userId ) {
-		/*final UserLockModel model = new UserLockModel( userId );
-
-		final User user = userService.load( userId );
-
-		model.setUserName( user.getNameEscaped() );
-		model.setUserCardLink( entityLinkUtilsService.getUserCardLink( user, EnvironmentContext.getLanguage() ) );*/
 
 		final List<UserLockHistoryDTO> result = newArrayList();
 
