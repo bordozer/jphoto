e7454f08fa8f91822619b21e6dfad285f439d217
bordozer <bordozer@gmail.com>
2014-04-19 16:22:36 +0300

    Postponed photos loading: admin special flags

--- jphoto/src/json/photo/PhotoEntryController.java
+++ jphoto/src/json/photo/PhotoEntryController.java
@@ -102,7 +102,7 @@ public class PhotoEntryController {
 		photoEntry.setShowAdminFlag_Anonymous( photo.isAnonymousPosting() && ( securityService.isSuperAdminUser( getCurrentUser() ) || securityService.userOwnThePhoto( getCurrentUser(), photo ) ) );
 		photoEntry.setShowAdminFlag_AnonymousTitle( translatorService.translate( "The photo is posted anonymously", getLanguage() ) );
 
-		photoEntry.setShowAdminFlag_Nude( photo.isAnonymousPosting() && ( securityService.isSuperAdminUser( getCurrentUser() ) || securityService.userOwnThePhoto( getCurrentUser(), photo ) ) );
+		photoEntry.setShowAdminFlag_Nude( photo.isContainsNudeContent() && securityService.isSuperAdminUser( getCurrentUser() ) );
 		photoEntry.setShowAdminFlag_NudeTitle( translatorService.translate( "The photo has nude content", getLanguage() ) );
 
 		return photoEntry;
