eb9f3fd5784bf89c6c9177c1b7c8dd98e349c9bb
bordozer <bordozer@gmail.com>
2014-05-01 11:36:36 +0300

    Photo appraisal: user validaton

--- jphoto/src/json/photo/appraisal/PhotoAppraisalFormValidator.java
+++ jphoto/src/json/photo/appraisal/PhotoAppraisalFormValidator.java
@@ -1,9 +1,16 @@
 package json.photo.appraisal;
 
+import core.services.translator.TranslatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import ui.context.EnvironmentContext;
+import utils.UserUtils;
 
 public class PhotoAppraisalFormValidator implements Validator {
+
+	@Autowired
+	private TranslatorService translatorService;
 
 	@Override
 	public boolean supports( final Class<?> clazz ) {
@@ -13,5 +20,14 @@ public class PhotoAppraisalFormValidator implements Validator {
 	@Override
 	public void validate( final Object target, final Errors errors ) {
 		final PhotoAppraisalDTO dto = ( PhotoAppraisalDTO ) target;
+
+		validateUser( dto.getUserId(), errors );
+	}
+
+	private void validateUser( final int userId, final Errors errors ) {
+		if ( ! UserUtils.isTheUserThatWhoIsCurrentUser( userId ) ) {
+			final String errorMessage = translatorService.translate( "You are not the user who has loaded appraisal form. The user must have changed since then. Please, reload the page.", EnvironmentContext.getLanguage() );
+			errors.rejectValue( "userId", "error code", errorMessage );
+		}
 	}
 }
