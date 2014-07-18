d630e2e3b26177218a49a3844ce016820fd21bd8
blu <blu@encode.dk>
2014-07-18 15:08:47 +0300

    Photo uploading: User team: 21 - deletion 4

--- jphoto/src/rest/photo/upload/userTeam/UserTeamController.java
+++ jphoto/src/rest/photo/upload/userTeam/UserTeamController.java
@@ -70,6 +70,8 @@ public class UserTeamController {
 //		teamMember.setTeamMemberUser(  ); // TODO
 
 		userTeamService.save( teamMember );
+
+		userTeamMemberDTO.setUserTeamMemberId( teamMember.getId() );
 	}
 
 	private List<UserTeamMemberDTO> getUserTeamMemberDTOs( final int photoId ) {
