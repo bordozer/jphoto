e15bd52791bcb0ac7f62e276393b5635a6ccc014
bordozer <bordozer@gmail.com>
2014-08-16 12:37:55 +0300

    Tests fix 5

--- jphoto/tests/unit/menuItems/photo/admin/PhotoAdminSubMenuItemLockUserTest.java
+++ jphoto/tests/unit/menuItems/photo/admin/PhotoAdminSubMenuItemLockUserTest.java
@@ -55,7 +55,7 @@ public class PhotoAdminSubMenuItemLockUserTest extends AbstractPhotoMenuItemTest
 
 		final User photoAuthor = testData.getPhotoAuthor();
 
-		assertEquals( EXPECTED_AND_ACTUAL_RESULTS_ARE_DIFFERENT, command.getMenuText(), translated(  String.format( "PhotoMenuItem: Lock photo author: %s", photoAuthor.getNameEscaped() ) ) );
+		assertEquals( EXPECTED_AND_ACTUAL_RESULTS_ARE_DIFFERENT, command.getMenuText(), translated(  String.format( "PhotoMenuItem: Restrict photo author: %s", photoAuthor.getNameEscaped() ) ) );
 		assertEquals( EXPECTED_AND_ACTUAL_RESULTS_ARE_DIFFERENT, command.getMenuCommand(), String.format( "adminRestrictUser( %d, '%s' );", photoAuthor.getId(), photoAuthor.getNameEscaped() ) );
 	}
 
