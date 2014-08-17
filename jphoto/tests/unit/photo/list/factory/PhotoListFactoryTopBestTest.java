06ef0264ff85663b87a4e5f6a71371f9263f9613
bordozer <bordozer@gmail.com>
2014-08-17 12:14:05 +0300

    PhotoListFactoryTopBestTest galleryTopBestForUserTest

--- jphoto/tests/unit/photo/list/factory/PhotoListFactoryTopBestTest.java
+++ jphoto/tests/unit/photo/list/factory/PhotoListFactoryTopBestTest.java
@@ -7,6 +7,7 @@ import core.general.data.PhotoSort;
 import core.general.genre.Genre;
 import core.general.photo.group.PhotoGroupOperationMenu;
 import core.general.photo.group.PhotoGroupOperationMenuContainer;
+import core.general.user.User;
 import core.services.entry.GroupOperationService;
 import core.services.photo.PhotoListCriteriasService;
 import core.services.photo.PhotoService;
@@ -52,6 +53,31 @@ public class PhotoListFactoryTopBestTest extends AbstractTestCase {
 		assertEquals( "Assertion fails", "", photoList.getBottomText() );
 		assertEquals( "Assertion fails", "http://127.0.0.1:8085/worker/photos/from/2014-08-13/to/2014-08-15/best/", photoList.getLinkToFullList() );
 		assertEquals( "Assertion fails", "Top best photo list description Top best photo list title: appraised from to 2014-08-13 - 2014-08-15", photoList.getPhotosCriteriasDescription() );
+		assertEquals( "Assertion fails", "2000,2001,2002,2003,2004,2005", StringUtils.join( photoList.getPhotoIds(), "," ) );
+	}
+
+	@Test
+	public void galleryTopBestForUserTest() {
+
+		final User user = new User();
+		user.setId( 112 );
+		user.setName( "Just a user" );
+
+		final TestData testData = new TestData();
+		testData.currentTime = dateUtilsService.parseDateTime( "2014-08-15 11:38:45" );
+		testData.photoIds = newArrayList( 2000, 2001, 2002, 2003, 2004, 2005 );
+		testData.user = user;
+
+		final ServicesImpl services = getTestServices( testData );
+
+		final AbstractPhotoListFactory photoListFactoryTopBest = new PhotoListFactoryTopBest( testData.user, testData.accessor, services );
+		final PagingModel pagingModel = new PagingModel( services );
+		final PhotoList photoList = photoListFactoryTopBest.getPhotoList( 0, pagingModel, Language.EN, testData.currentTime );
+
+		assertEquals( "Assertion fails", "Top best photo list title Top best photo list title: of member <a class=\"member-link\" href=\"http://127.0.0.1:8085/worker/members/112/card/\" title=\"EntityLinkUtilsService: Just a user: user card link title\">Just a user</a> Top best photo list title: appraised from to 2014-08-13 - 2014-08-15", photoList.getPhotoListTitle() );
+		assertEquals( "Assertion fails", "", photoList.getBottomText() );
+		assertEquals( "Assertion fails", "http://127.0.0.1:8085/worker/photos/members/112/best/", photoList.getLinkToFullList() );
+		assertEquals( "Assertion fails", "Top best photo list description Top best photo list title: of member <a class=\"member-link\" href=\"http://127.0.0.1:8085/worker/members/112/card/\" title=\"EntityLinkUtilsService: Just a user: user card link title\">Just a user</a> Top best photo list title: appraised from to 2014-08-13 - 2014-08-15", photoList.getPhotosCriteriasDescription() );
 		assertEquals( "Assertion fails", "2000,2001,2002,2003,2004,2005", StringUtils.join( photoList.getPhotoIds(), "," ) );
 	}
 
