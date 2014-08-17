384693a385017c976ec1585cc31853ce55657630
bordozer <bordozer@gmail.com>
2014-08-17 12:16:25 +0300

    PhotoListFactoryTopBestTest galleryTopBestForUserAndGenresTest

--- jphoto/tests/unit/photo/list/factory/PhotoListFactoryTopBestTest.java
+++ jphoto/tests/unit/photo/list/factory/PhotoListFactoryTopBestTest.java
@@ -106,6 +106,36 @@ public class PhotoListFactoryTopBestTest extends AbstractTestCase {
 		assertEquals( "Assertion fails", "2000,2001,2002,2003,2004,2005", StringUtils.join( photoList.getPhotoIds(), "," ) );
 	}
 
+	@Test
+	public void galleryTopBestForUserAndGenresTest() {
+
+		final User user = new User();
+		user.setId( 112 );
+		user.setName( "Just a user" );
+
+		final Genre genre = new Genre();
+		genre.setId( 222 );
+		genre.setName( "Glamour" );
+
+		final TestData testData = new TestData();
+		testData.currentTime = dateUtilsService.parseDateTime( "2014-08-15 11:38:45" );
+		testData.photoIds = newArrayList( 2000, 2001, 2002, 2003, 2004, 2005 );
+		testData.user = user;
+		testData.genre = genre;
+
+		final ServicesImpl services = getTestServices( testData );
+
+		final AbstractPhotoListFactory photoListFactoryTopBest = new PhotoListFactoryTopBest( testData.user, testData.genre, testData.accessor, services );
+		final PagingModel pagingModel = new PagingModel( services );
+		final PhotoList photoList = photoListFactoryTopBest.getPhotoList( 0, pagingModel, Language.EN, testData.currentTime );
+
+		assertEquals( "Assertion fails", "Top best photo list title Top best photo list title: of member <a class=\"member-link\" href=\"http://127.0.0.1:8085/worker/members/112/card/\" title=\"EntityLinkUtilsService: Just a user: user card link title\">Just a user</a> Top best photo list title: in category Translated entry Top best photo list title: appraised from to 2014-08-13 - 2014-08-15", photoList.getPhotoListTitle() );
+		assertEquals( "Assertion fails", "", photoList.getBottomText() );
+		assertEquals( "Assertion fails", "http://127.0.0.1:8085/worker/photos/members/112/genre/222/best/", photoList.getLinkToFullList() );
+		assertEquals( "Assertion fails", "Top best photo list description Top best photo list title: of member <a class=\"member-link\" href=\"http://127.0.0.1:8085/worker/members/112/card/\" title=\"EntityLinkUtilsService: Just a user: user card link title\">Just a user</a> Top best photo list title: in category Translated entry Top best photo list title: appraised from to 2014-08-13 - 2014-08-15", photoList.getPhotosCriteriasDescription() );
+		assertEquals( "Assertion fails", "2000,2001,2002,2003,2004,2005", StringUtils.join( photoList.getPhotoIds(), "," ) );
+	}
+
 	private ServicesImpl getTestServices( final TestData testData ) {
 		final ServicesImpl services = getServices();
 
@@ -177,6 +207,7 @@ public class PhotoListFactoryTopBestTest extends AbstractTestCase {
 		EasyMock.expect( photoListCriteriasService.getForAllPhotosTopBest( testData.accessor ) ).andReturn( criterias ).anyTimes();
 		EasyMock.expect( photoListCriteriasService.getForGenreTopBest( testData.genre, testData.accessor ) ).andReturn( criterias ).anyTimes();
 		EasyMock.expect( photoListCriteriasService.getForUserTopBest( testData.user, testData.accessor ) ).andReturn( criterias ).anyTimes();
+		EasyMock.expect( photoListCriteriasService.getForUserAndGenreTopBest( testData.user, testData.genre, testData.accessor ) ).andReturn( criterias ).anyTimes();
 
 		EasyMock.expect( photoListCriteriasService.getLinkToFullListText( criterias ) ).andReturn( "Link To Full List Text" ).anyTimes();
 
