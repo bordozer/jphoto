178c3e8d211e54e226412364039f1a5169968f4e
blu <blu@encode.dk>
2015-02-13 10:54:23 +0200

    MembersInvisibilityList 6.1

--- jphoto/src/core/services/photo/list/PhotoListFactoryService.java
+++ jphoto/src/core/services/photo/list/PhotoListFactoryService.java
@@ -1,7 +1,6 @@
 package core.services.photo.list;
 
 import core.enums.FavoriteEntryType;
-import core.general.data.TimeRange;
 import core.general.genre.Genre;
 import core.general.photo.PhotoVotingCategory;
 import core.general.user.User;
